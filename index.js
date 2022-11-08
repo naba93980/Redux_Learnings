807d1e8733acf4cb01470d435ab1a53734ad8b47
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-08 22:11:26 +0530

    added restoreCake fn to dispatch new action object

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
 
 // --------- store object -------
 const initialState = {
@@ -14,13 +15,30 @@ function orderCake() {
     };
 }
 
+function restoreCake() {
+    return {
+        type: CAKE_RESTOCKED,
+        quantity: 10,
+    }
+}
+
 // -------- reducer function -------
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case CAKE_ORDERED: return ({
             ...state,
-            numOfCakes: numOfCakes - 1
+            numOfCakes: state.numOfCakes - 1
+        });
+        case CAKE_RESTOCKED: return ({
+            ...state,
+            numOfCakes: action.quantity
         });
         default: return state;
     }
+}
+
+module.exports = {
+    reducer,
+    orderCake,
+    restoreCake
 }
