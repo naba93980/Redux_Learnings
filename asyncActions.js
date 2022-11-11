8ecc486739c2169307e42b76364a0937d436dc21
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-11 16:52:00 +0530

    async action creators using redux-thunk

---
+++ asyncActions.js
@@ -0,0 +1,80 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const axios = require('axios');
+const thunkMiddleware = require('redux-thunk').default;
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+const FETCH_USERS_REQUESTED = 'FETCH_USERS_REQUESTED'
+const FETCH_USERS_SUCCEEDED = 'FETCH_USERS_SUCCEEDED'
+const FETCH_USERS_FAILED = 'FETCH_USERS_FAILED'
+
+
+const fetchUsersRequest = () => {
+    return {
+        type: FETCH_USERS_REQUESTED,
+    }
+}
+const fetchUsersSuccess = (users) => {
+    return {
+        type: FETCH_USERS_SUCCEEDED,
+        payload: users,
+    }
+}
+const fetchUsersFailure = (error) => {
+    return {
+        type: FETCH_USERS_FAILED,
+        payload: error,
+    }
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_USERS_REQUESTED: return {
+            ...state,
+            loading: true,
+        }
+        case FETCH_USERS_SUCCEEDED: return {
+            ...state,
+            loading: false,
+            users: action.payload,
+            error: '',
+        }
+        case FETCH_USERS_FAILED: return {
+            ...state,
+            loading: false,
+            users: [],
+            error: action.payload,
+        }
+    }
+}
+
+const fetchUsers = () => {
+    return function (dispatch) {
+
+        console.log("entered fetchUsers function");
+        dispatch(fetchUsersRequest());
+
+        axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+            const users = response.data.map(user => user.id)
+            dispatch(fetchUsersSuccess(users));
+        }).catch(err => {
+            dispatch(fetchUsersFailure(err.message));
+        })
+    }
+}
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+})
+
+store.dispatch(fetchUsers())
+store.dispatch(fetchUsers())
+
+// unsubscribe();
